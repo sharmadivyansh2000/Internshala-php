45399b783097e4df6746c825df643b886fac6e58
sharmadivyansh2000 <69228321+sharmadivyansh2000@users.noreply.github.com>
2020-08-05 08:21:57 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,38 @@
+<?php
+	require ("common.php");
+	
+	
+	if(!isset($_SESSION['email'])){
+		header("location: index.php");
+	}else{
+		session_unset();
+		session_destroy();
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>LifeStyle Stores</title>
+		
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+		
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		
+		<link rel="stylesheet" type="text/css" href="index.css">
+	</head>
+	<body>
+		<?php
+                include 'header.php';
+		?>
+        <div class="container panel-margin">
+            <div class="alert alert-success">
+                You have successfully logged out. Click <a href="index.php">here</a> to go back to index page.
+            </div>
+        </div>
+		<?php
+                include 'footer.php'; 
+		?>
+	</body>
+</html>
---
+++ settings_script.php
@@ -0,0 +1,26 @@
+<?php
+	require ("common.php");
+	
+	if(!isset($_SESSION['email'])){
+		header("location: index.php");
+    }else{
+        
+        $user_id = $_SESSION["id"];
+        $email = $_SESSION["email"];
+
+       
+        $new_password = md5(mysqli_real_escape_string($conn, $_POST["new_pass"]));
+        $retype_pass = md5(mysqli_real_escape_string($conn, $_POST["retype_new_pass"]));
+
+        
+        if($new_password != $retype_pass){
+            echo "The passwords do not match. Try again.";
+        }else{
+            $query = "UPDATE users SET password = '$new_password' WHERE email = '$email' AND id = '$user_id'";
+
+            $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
+
+            header("location: settings.php");
+        }
+    }
+?>
